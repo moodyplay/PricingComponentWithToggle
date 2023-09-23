67c2a180b0c4b0a78965d71a1f9811f83a4f9ee6
PatrykHensa <patrykhensa@gmail.com>
2023-09-23 13:17:03 +0200

    finished changing works property

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const switchEl = document.getElementById("switch");
 const cardsEl = document.getElementsByClassName("card");
 
-const cssTemplateString = `.foo:[:before]{prop: opacity:1}`;
+
 
 switchEl.addEventListener('change', function () {
-
-
-
-
-    
     for (let i = 0; i < cardsEl.length; i++) {
-        cardsEl[i].innerHTML = cssTemplateString;
+        const card = cardsEl[i];
+        const before = card.querySelector("::before");
         
+        if (switchEl.checked) {
+            card.classList.add("blue"); // Add a class to control the opacity
+        } else {
+            card.classList.remove("blue"); // Remove the class to control the opacity
+        }
     }
-});    
+});
