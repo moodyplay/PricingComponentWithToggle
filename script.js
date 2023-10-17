0decf5cec95e7feb0c7f9f814faf209d6da157ca
PatrykHensa <patrykhensa@gmail.com>
2023-10-17 21:49:41 +0200

    resposivity

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const switchEl = document.getElementById("switch");
 const cardsEl = document.getElementsByClassName("card");
-
+const priceEl = document.getElementsByClassName("price-el");
 
 
 switchEl.addEventListener('change', function () {
     for (let i = 0; i < cardsEl.length; i++) {
         const card = cardsEl[i];
         const before = card.querySelector("::before");
+        const price = priceEl[i];
+        const priceElement = parseFloat(price.textContent);
         
         if (switchEl.checked) {
             card.classList.add("blue"); // Add a class to control the opacity
+            price.textContent = (priceElement * 12);
+
         } else {
             card.classList.remove("blue"); // Remove the class to control the opacity
+            price.textContent = (priceElement * (1/12));
         }
     }
 });
